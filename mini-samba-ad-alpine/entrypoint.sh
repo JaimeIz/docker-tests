ad7f9b651842edea931b16fbf57bcf6a3c18fa6c
jaimeiz <jaimeiz@archer>
2024-05-26 13:25:53 +0200

    Added phpldapadmin (Not yet working)

--- mini-samba-ad-alpine/entrypoint.sh
+++ mini-samba-ad-alpine/entrypoint.sh
@@ -5,14 +5,20 @@ if [ -z "$NETBIOS_NAME" ]; then
 else
   NETBIOS_NAME=$(echo $NETBIOS_NAME | tr [a-z] [A-Z])
 fi
+
 REALM=$(echo "$REALM" | tr [a-z] [A-Z])
 WORKGROUP=$(echo "$WORKGROUP" | tr [a-z] [A-Z])
+
+if [ "$LDAP_ALLOW_INSECURE" != "true" ]; then
+  LDAP_ALLOW_INSECURE=''
+fi
 
 if [ ! -f /etc/timezone ] && [ ! -z "$TZ" ]; then
   echo 'Set timezone'
   cp /usr/share/zoneinfo/$TZ /etc/localtime
   echo $TZ >/etc/timezone
 fi
+
 
 if [ ! -f /var/lib/samba/registry.tdb ]; then
   if [ ! -f /run/secrets/$ADMIN_PASSWORD_SECRET ]; then
@@ -22,25 +28,15 @@ if [ ! -f /var/lib/samba/registry.tdb ]; then
 
   ADMIN_PASSWORD=$(cat /run/secrets/$ADMIN_PASSWORD_SECRET)
   if [ "$BIND_INTERFACES_ONLY" == yes ]; then
-    INTERFACE_OPTS="--option=\"bind interfaces only=yes\" \
-      --option=\"interfaces=$INTERFACES\""
+    INTERFACE_OPTS="--option=\"bind interfaces only=yes\" --option=\"interfaces=$INTERFACES\""
   fi
   
-  if [ $DOMAIN_ACTION == provision ]; then
-    PROVISION_OPTS="--server-role=dc \
-      --use-rfc2307 --domain=$WORKGROUP --realm=$REALM --adminpass='$ADMIN_PASSWORD'"
-  elif [ $DOMAIN_ACTION == join ]; then
-    PROVISION_OPTS="$REALM DC -UAdministrator --password='$ADMIN_PASSWORD'"
-  else
-    echo 'Only provision and join actions are supported.'
-    exit 1
-  fi
+  PROVISION_OPTS="--server-role=dc --use-rfc2307 --domain=$WORKGROUP --realm=$REALM --adminpass='$ADMIN_PASSWORD'"
   
   rm -f /etc/samba/smb.conf /etc/krb5.conf
 
   # This step is required for INTERFACE_OPTS to work as expected
-  echo "samba-tool domain $DOMAIN_ACTION $PROVISION_OPTS $INTERFACE_OPTS \
-    --dns-backend=SAMBA_INTERNAL" | sh
+  echo "samba-tool domain $DOMAIN_ACTION $PROVISION_OPTS $INTERFACE_OPTS --dns-backend=SAMBA_INTERNAL" | sh
 
   mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
   echo 'root = administrator' > /etc/samba/smbusers
